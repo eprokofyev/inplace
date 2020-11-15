87607f7777b5866e39b4155b87b2c8d0cb38a848
Mahmudjon Rahimov <mrahimov72@gmail.com>
2020-11-15 17:05:49 +0300

    Made a layouts for message item.Wrote the logic for setting the message item

--- app/src/main/java/com/inplace/ChatFragment.kt
+++ app/src/main/java/com/inplace/ChatFragment.kt
@@ -1,9 +1,9 @@
 package com.inplace
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.*
 import android.widget.EditText
 import android.widget.ImageView
@@ -11,26 +11,28 @@ import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 
 class ChatFragment() : Fragment() {
 
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        Log.d("myLogs", "onCreateView")
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
         setHasOptionsMenu(true)
         return inflater.inflate(R.layout.chat_fragment, container, false)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        Log.d("myLogs", "onCreateOptionsMenu")
         inflater.inflate(R.menu.chat_menu, menu)
         super.onCreateOptionsMenu(menu, inflater)
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        Log.d("myLogs", "onViewCreated")
         super.onViewCreated(view, savedInstanceState)
 
         //testing changing info about user in toolbar
@@ -39,39 +41,117 @@ class ChatFragment() : Fragment() {
         val userActivity: TextView = toolbar.findViewById(R.id.chat_user_activity)
         username.text = "Aiden Pierce"
         userActivity.text = "last seen 5 minutes ago"
-        //up here)
-
 
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+
+        //testing message view
+        val targetMessage = "Привет!\n" +
+                "Предлагаю в воскресенье собраться в дисе, обсудить архитектуру и раскидать задачи. К воскресенью давайте закончим с текущими задачами и посмотрим повнимательнее материалы лекций, чтобы понимать о чём пойдёт речь"
+        val hostMessage = "Окей"
+        val targetMessageView: View = view.findViewById(R.id.target_message_test)
+        ChatFragment.setMessageText(context, targetMessageView, targetMessage)
+        val hostMessageView: View = view.findViewById(R.id.host_message_test)
+        ChatFragment.setMessageText(context, hostMessageView, hostMessage)
+
+
 
         val messageEditText: EditText = view.findViewById(R.id.chat_message_edittext)
         val sendButton: RelativeLayout = view.findViewById(R.id.chat_send_button)
         val voiceButton: ImageView = view.findViewById(R.id.chat_voice_button)
 
+        sendButton.setOnClickListener {
+            ChatFragment.setMessageText(context, hostMessageView, messageEditText.text.toString())
+            messageEditText.setText("")
+        }
+
         //Adding a watcher to replace the send/voice buttons
-        messageEditText.addTextChangedListener(object: TextWatcher{
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                Log.d("myLogs", ("CharSequence: "+s?.length) as String)
-                when{
-                    s?.length?:0 > 0 -> {
-                        sendButton.visibility = View.VISIBLE
-                        voiceButton.visibility = View.GONE
-                    }
-
-                    s?.length?:0 < 1 -> {
-                        sendButton.visibility = View.GONE
-                        voiceButton.visibility = View.VISIBLE
-                    }
+        messageEditText.doOnTextChanged { text, _, _, _ ->
+            when(text?.length?:0) {
+                0 -> {
+                    sendButton.visibility = View.GONE
+                    voiceButton.visibility = View.VISIBLE
+                }
+                else -> {
+                    sendButton.visibility = View.VISIBLE
+                    voiceButton.visibility = View.GONE
                 }
             }
-            override fun afterTextChanged(s: Editable?) {
-            }
+        }
 
-        })
     }
+
+    companion object {
+        /**
+         * Sets the text of the message
+         * to the View consisting of two TextViews
+         */
+        fun setMessageText(context: Context?, messageView: View, message: String) {
+            val upperMessageTextView: TextView = messageView.findViewById(R.id.upper_message_textView)
+            val lowerMessageTextView: TextView = messageView.findViewById(R.id.lower_message_textView)
+
+            val messageLines: MutableList<CharSequence> = ArrayList()
+            var messageLinesCount: Int
+            var isSet = false
+            var upperTextViewResult = ""
+
+            upperMessageTextView.text = ""
+            lowerMessageTextView.text = ""
+            upperMessageTextView.visibility = View.VISIBLE
+            lowerMessageTextView.visibility = View.VISIBLE
+
+            if (context != null) {
+                val lowerMessageMaxLength = context.resources.getInteger(R.integer.lower_message_maxLength)
+
+                upperMessageTextView.text = message
+                upperMessageTextView.viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
+                    override fun onPreDraw(): Boolean {
+                        upperMessageTextView.viewTreeObserver.removeOnPreDrawListener(this)
+
+                        if (!isSet) {
+                            messageLinesCount = upperMessageTextView.layout.lineCount
+
+                            repeat(messageLinesCount){line ->
+                                val start = upperMessageTextView.layout.getLineStart(line)
+                                val end = upperMessageTextView.layout.getLineEnd(line)
+                                val substring = upperMessageTextView.text.subSequence(start, end)
+                                messageLines.add(substring)
+                            }
+
+                            messageLines.forEachIndexed { i, it ->
+                                if (i == messageLinesCount - 1) {
+                                    when {
+                                        it.length <= lowerMessageMaxLength -> {
+                                            if (messageLinesCount > 1) {
+                                                upperMessageTextView.text = upperTextViewResult
+                                                lowerMessageTextView.text = it
+                                                isSet = true
+                                            } else {
+                                                upperMessageTextView.text = ""
+                                                upperMessageTextView.visibility = View.GONE
+                                                lowerMessageTextView.text = it
+                                                isSet = true
+                                            }
+                                        }
+
+                                        else -> {
+                                            upperTextViewResult += it
+                                            upperMessageTextView.text = upperTextViewResult
+                                            lowerMessageTextView.visibility = View.GONE
+                                            isSet = true
+                                        }
+
+                                    }
+                                } else {
+                                    upperTextViewResult += it
+                                }
+                            }
+                        }
+                        return isSet
+                    }
+                })
+            }
+        }
+    }
+
 }
