c6d66b5d4b1f048a11603b94e90b1c7be2a7f6da
AlekseySirmais7 <aleksei.sirmais@yandex.ru>
2020-11-16 17:21:33 +0300

    start work with api

---
+++ app/src/main/java/com/inplace/api/vk/ApiVK.java
@@ -0,0 +1,12 @@
+package com.inplace.api.vk;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class ApiVK {
+
+    private static final String apiHostAuth = "https://oauth.vk.com/";
+    private static final String apiHost = "https://api.vk.com/";
+
+
+}
