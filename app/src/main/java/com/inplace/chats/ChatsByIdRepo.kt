b31a45db408030aade1312cbc7e6cba5587a689f
a_luz <artyom.luzgin@mail.ru>
2020-12-27 14:23:41 +0300

    Foreground Service fix

---
+++ app/src/main/java/com/inplace/chats/ChatsByIdRepo.kt
@@ -0,0 +1,139 @@
+package com.inplace.chats
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.inplace.api.vk.*
+import com.inplace.api.vk.VkChat.CHAT_TYPE_USER
+import com.inplace.chats.models.Avatar
+import com.inplace.models.*
+import com.inplace.services.ExecutorServices
+import com.vk.api.sdk.VK
+import java.lang.Exception
+import java.util.ArrayList
+import java.util.concurrent.Executor
+
+class ChatsByIdRepo {
+
+    private val avatars: MutableLiveData<MutableList<Avatar>> = MutableLiveData<MutableList<Avatar>>()
+
+    //private val loader = ApiImageLoader.getInstance(context)
+
+    private val executor: Executor = ExecutorServices.getInstanceAPI()
+
+    fun refresh(ids: ArrayList<Int>): MutableList<VKChat> {
+        val result = getChatsFromVK(ids)
+        return if (result is ResultVKChat.Success) {
+            Log.d("tag", result.data.size.toString())
+            result.data
+        } else {
+            mutableListOf()
+        }
+    }
+
+
+    private fun getChatsFromVK(ids: ArrayList<Int>): ResultVKChat {
+        val chats = mutableListOf<VKChat>()
+        val result = ApiVk.getChatsById(ids)
+
+        Log.d("status","do")
+
+        if (result.error != null) {
+            return ResultVKChat.Error(Exception(result.errTextMsg))
+        }
+
+        val vkChats = result.result?.chats ?: arrayListOf<VkChat>()
+        val vkUsers = result.result?.users ?: hashMapOf<Int, VkUser>()
+
+
+        for (vkChat in vkChats) {
+
+            val msg = Message(
+                vkChat.lasMsgId,
+                vkChat.date.toLong(),
+                vkChat.text,
+                vkChat.lastMsgFromId.toLong(),
+                vkChat.chatWithId.toLong(),
+                vkChat.lastMsgFromId == VK.getUserId(),
+                Source.VK,
+                false,
+                arrayListOf(),
+            )
+
+            var vkUser = vkUsers[vkChat.lastMsgFromId]
+            if (vkUser == null) {
+                return ResultVKChat.Error(Exception("собеседник не найден"))
+            }
+
+            var sobesednik = VKSobesednik(
+                vkUser.id.toLong(),
+                vkUser.firstName,
+                vkUser.lastName,
+                null,
+                vkUser.photo200Square,
+                if (vkUser.online) "online" else "",
+                "about",
+                0,
+            )
+
+            val usersMap = hashMapOf<Long, IVKSobesednik>()
+            var title = vkChat.groupChatTitle
+            var avatarUrl = ""
+            var chatType = ChatType.GROUP
+            if (vkChat.chatType == CHAT_TYPE_USER) {
+                if (sobesednik.userID != vkChat.chatWithId.toLong()) {
+                    vkUser = vkUsers[vkChat.chatWithId]
+                    if (vkUser == null) {
+                        return ResultVKChat.Error(Exception("собеседник не найден"))
+                    }
+
+                    sobesednik = VKSobesednik(
+                        vkUser.id.toLong(),
+                        vkUser.firstName,
+                        vkUser.lastName,
+                        null,
+                        vkUser.photo200Square,
+                        if (vkUser.online) "online" else "",
+                        "about",
+                        0,
+                    )
+                }
+
+                title = sobesednik.name + " " + sobesednik.lastName
+                avatarUrl = sobesednik.avatarUrl
+                chatType = ChatType.PRIVATE
+            }
+
+            val chat = VKChat(
+                vkChat.chatWithId.toLong(),
+                Source.VK,
+                title,
+                null,
+                avatarUrl,
+                arrayListOf(msg),
+                true,
+                chatType,
+                msg,
+                hashMapOf<Long, IVKSobesednik>(sobesednik.userID to SuperSobesednik(sobesednik, null, Source.VK)),
+            )
+
+            chats.add(chat)
+        }
+
+        Log.d("status","chats " + chats.size.toString())
+
+        return ResultVKChat.Success(chats)
+    }
+
+    fun getAvatars() = avatars
+
+    interface OnRequestCompleteListener {
+        fun onRequestComplete(result: ResultVKChat)
+    }
+
+}
+
+//sealed class ResultVKChat {
+//    data class Success(val data: MutableList<VKChat>) : ResultVKChat()
+//    data class Error(val error: Exception) : ResultVKChat()
+//}
--- app/src/main/java/com/inplace/services/NotificationService.kt
+++ app/src/main/java/com/inplace/services/NotificationService.kt
@@ -1,12 +1,10 @@
 package com.inplace.services
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.app.Service
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Build
 import android.os.Handler
 import android.os.IBinder
@@ -18,8 +16,11 @@ import androidx.paging.ExperimentalPagingApi
 import com.inplace.MainActivity
 import com.inplace.R
 import com.inplace.api.vk.ApiVk
+import com.inplace.chats.ChatsByIdRepo
+import com.inplace.chats.ChatsRepo
 import com.inplace.db.AppDatabase
 import com.inplace.models.SuperChat
+import com.inplace.models.VKChat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -32,6 +33,7 @@ class NotificationService : Service() {
             val startIntent = Intent(context, NotificationService::class.java)
             ContextCompat.startForegroundService(context, startIntent)
         }
+
         fun stopService(context: Context) {
             val stopIntent = Intent(context, NotificationService::class.java)
             context.stopService(stopIntent)
@@ -42,24 +44,37 @@ class NotificationService : Service() {
         return null
     }
 
-    private val NOTIFICATION_ID_MESSAGE = 1
+    override fun onCreate() {
+        val context = this as Context
+    }
+
+    private val NOTIFICATION_ID_FOREGROUND = 1
+    private val NOTIFICATION_ID_MESSAGE = 2
     private val LIGHT_COLOR_ARGB = R.color.purple_500
     private val CHANNEL_MESSAGES = "messages"
+    private val CHANNEL_FOREGROUND = "foreground"
     private val CHANNEL_NAME = "Новое сообщение"
+    private val CHANNEL_FOREGROUND_NAME = "Состояние"
     private var mMessageCount = 0
-    lateinit var mManager : NotificationManager
-    lateinit var notificationChannel: NotificationChannel
-    //private val intentToActivity = Intent(this, MainActivity::class.java)
+    lateinit var mManager: NotificationManager
+    private val intentToActivity = Intent(this, MainActivity::class.java)
 
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        //       val database = AppDatabase.getInstance(this)
+        createNotificationChannel()
+        val builderForForeground = NotificationCompat.Builder(this, CHANNEL_FOREGROUND)
+            .setSmallIcon(R.drawable.ic_tick)
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText(getString(R.string.for_foreground))
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setColor(resources.getColor(R.color.purple_500))
+        startForeground(NOTIFICATION_ID_FOREGROUND, builderForForeground.build())
+
         //Уведомления
         // Подписка на новые сообщения
 
-
-
         ExecutorServices.getInstanceAPI().execute() {
+
             //    CoroutineScope(Dispatchers.IO).launch {
             while (true) {
                 val newMesgsResult = ApiVk.getNewMessages()
@@ -69,47 +84,43 @@ class NotificationService : Service() {
                     continue
                 }
 
-
-
-
                 val newMessagesArray = newMesgsResult.result
+                if (newMessagesArray.isNotEmpty()) {
+                    val chatIds: ArrayList<Int> = ArrayList()
 
-                for (el in newMessagesArray) {
-                    showMessageNotification(el.text)
-//                    val vkChat = database.getGetChatDao().getChat(el.chatID)
-//                    val myIntent = Intent(this@NotificationService, MainActivity::class.java)
-//                    vkChat.let {
-//                        SuperChat(
-//                            it.title,
-//                            it.avatarUrl,
-//                            it.lastMessage,
-//                            true,
-//                            arrayListOf(it),
-//                            arrayListOf(),
-//                            it.chatID,
-//                            it.chatID
-//                        )
-//                    }
-//                    myIntent.putExtra("dfsd",vkChat)
-//                    sendBroadcast(myIntent);
+                    for (el in newMessagesArray) {
+                        showMessageNotification(el.text)
+                        chatIds.add(el.chatID.toInt())
+                    }
+                    for (i in chatIds) {
+                        Log.d("ApiVK", i.toString())
+                    }
+                    val repo = ChatsByIdRepo()
+                    val vkChats = repo.refresh(chatIds)
+                    if (vkChats.isNotEmpty()) {
+                        Log.d("ApiVK", "end of get chats by id")
+                        continue
+                    }
+                    val vkChatsArray = vkChats.toTypedArray()
+                    val myIntent = Intent(this@NotificationService, MainActivity::class.java)
+                    myIntent.putExtra("dfsd", vkChatsArray)
+                    sendBroadcast(myIntent);
                 }
                 Thread.sleep(3000)
             }
         }
-        return START_NOT_STICKY
-        //   return super.onStartCommand(intent, flags, startId)
+        return START_REDELIVER_INTENT
     }
 
     private fun showMessageNotification(messageToShow: String) {
-        /*val pendingIntent = PendingIntent.getActivity(
-            this,
+        val pendingIntent = PendingIntent.getActivity(
+            this as Context,
             0,
             intentToActivity,
             PendingIntent.FLAG_UPDATE_CURRENT
         )
-         */
+
         mMessageCount++
-        createNotificationChannel()
         val largeIcon = BitmapFactory.decodeResource(resources, R.drawable.foto)
         val builder = NotificationCompat.Builder(this, CHANNEL_MESSAGES)
             .setLargeIcon(largeIcon)
@@ -120,20 +131,28 @@ class NotificationService : Service() {
             .setLights(resources.getColor(LIGHT_COLOR_ARGB), 1000, 1000)
             .setColor(resources.getColor(R.color.purple_500))
             .setAutoCancel(true)
-        //.setContentIntent(pendingIntent)
+            .setContentIntent(pendingIntent)
         val style = NotificationCompat.BigTextStyle()
         style.bigText(messageToShow)
         builder.setStyle(style)
-        startForeground(NOTIFICATION_ID_MESSAGE, builder.build())
-        //   mManager.notify(NOTIFICATION_ID_MESSAGE, builder.build())
-    }
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val serviceChannel = NotificationChannel(CHANNEL_MESSAGES, CHANNEL_NAME,
-                NotificationManager.IMPORTANCE_HIGH)
-            mManager = getSystemService(NotificationManager::class.java)
-            mManager!!.createNotificationChannel(serviceChannel)
-        }
+        mManager.notify(NOTIFICATION_ID_MESSAGE, builder.build())
     }
 
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            mManager = getSystemService(NotificationManager::class.java)
+            val serviceChannel = NotificationChannel(CHANNEL_FOREGROUND, CHANNEL_FOREGROUND_NAME, NotificationManager.IMPORTANCE_NONE)
+            mManager.createNotificationChannel(serviceChannel)
+            val notificationChannel = NotificationChannel(
+                CHANNEL_MESSAGES, CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                enableLights(true)
+                enableVibration(true)
+                lightColor = LIGHT_COLOR_ARGB
+                lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+            }
+            mManager.createNotificationChannel(notificationChannel)
+        }
+    }
 }
